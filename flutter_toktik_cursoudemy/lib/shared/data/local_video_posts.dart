bb2decc72555cce9c03c7579d555ef9ba96faa5f
Mike19200 <83485035+Mike19200@users.noreply.github.com>
2024-02-01 00:12:07 -0500

    toktik app proyect and widget app proyect

---
+++ flutter_toktik_cursoudemy/lib/shared/data/local_video_posts.dart
@@ -0,0 +1,28 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    "name": "Se fue la luz - LATINMAFIA",
+    "videoUrl": "assets/videos/1.mp4",
+    "likes": 23230,
+    "views": 152223
+  },
+  {
+    "name": "Kali Uchis + Peso Pluma",
+    "videoUrl": "assets/videos/2.mp4",
+    "likes": 245230,
+    "views": 13343
+  },
+  {
+    "name": "Rosalia Motomami Tour",
+    "videoUrl": "assets/videos/3.mp4",
+    "likes": 21564320,
+    "views": 123563
+  },
+  {
+    "name": "Billie Eilish FEP 2023",
+    "videoUrl": "assets/videos/4.mp4",
+    "likes": 10020,
+    "views": 23200
+  },
+  
+];
+
